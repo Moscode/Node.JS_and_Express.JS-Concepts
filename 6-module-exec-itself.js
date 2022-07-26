29c3137e0f487c7e164df5a9ac3603a4c5215ac2
moscode <odunoshomoses@gmail.com>
2022-07-26 08:21:35 +0100

    [feat]: module execute directly without storing it

---
+++ 6-module-exec-itself.js
@@ -0,0 +1,8 @@
+const num1 = 10;
+const num2 = 10;
+
+const addVals = (num, nummy) => {
+    console.log(`Multiplication is: ${num1 * num2}`);
+}
+
+addVals(num1, num2);
--- App.js
+++ App.js
@@ -3,11 +3,12 @@
 const names = require('./3-names');
 const greeting = require('./4-utils')
 const data = require('./5-alternative-flavor-export')
+require('./6-module-exec-itself');
 
-greeting(names.new_name)
-greeting(names.new_name2)
-greeting(names.new_name3)
+//greeting(names.new_name)
+//greeting(names.new_name2)
+//greeting(names.new_name3)
 
-console.log(data)
+//console.log(data)
 
 // console.log(module) - helps you understand where the export is from and some other stuff
