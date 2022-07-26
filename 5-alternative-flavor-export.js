2bda8c79a1c5d97ed08d84f4d424dcd6184b1b80
moscode <odunoshomoses@gmail.com>
2022-07-26 08:15:02 +0100

    [feat]: alternative to exporting modules

---
+++ 5-alternative-flavor-export.js
@@ -0,0 +1,8 @@
+// This method is called export as you go
+module.exports.items = ["Dan", "Mike"]
+
+const animal = {
+    name: "Lion"
+}
+
+module.exports.singleAnimal = animal;
--- App.js
+++ App.js
@@ -2,7 +2,12 @@
 // Modules - Encapsulated Code (only share minimum)
 const names = require('./3-names');
 const greeting = require('./4-utils')
+const data = require('./5-alternative-flavor-export')
 
 greeting(names.new_name)
 greeting(names.new_name2)
 greeting(names.new_name3)
+
+console.log(data)
+
+// console.log(module) - helps you understand where the export is from and some other stuff
